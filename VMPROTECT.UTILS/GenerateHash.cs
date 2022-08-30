bd8795db0c9a56b5f91ad37d9352df9284596c5b
eaglx <eaglx.programmer@gmail.com>
2022-08-30 23:19:06 +0200

    add utils

---
+++ VMPROTECT.UTILS/GenerateHash.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace VMPROTECT.UTILS
+{
+    public static class GenerateHash
+    {
+        public static string ComputeSha256Hash(byte[] bytesData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha256Hash.ComputeHash(bytesData);
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                foreach (var b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ VMPROTECT.UTILS/ValidateCode.cs
@@ -0,0 +1,26 @@
+﻿using VMPROTECT.CORE.Security.Exceptions;
+using VMPROTECT.CORE.Security.Models;
+
+namespace VMPROTECT.UTILS
+{
+    public static class ValidateCode
+    {
+        public static void ValidateMagicNumber(byte[] code)
+        {
+            var magicNumber = (uint)((code[0] << 8) | code[1]);
+            if (magicNumber != SecDetails.MagicNumber)
+            {
+                throw new SecurityBreach("Invalid magic number");
+            }
+        }
+
+        public static void ValidateDataStructure(byte[] code)
+        {
+            var generatedHash256 = GenerateHash.ComputeSha256Hash(code);
+            if(!SecFile.CodeHash.Equals(generatedHash256))
+            {
+                throw new SecurityBreach("Invalid hash");
+            }
+        }
+    }
+}
